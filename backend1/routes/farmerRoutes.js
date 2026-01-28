195efe0934c0fa874d84e02f067a69e419bf33a7
krishidev1 <ghoshkrishidev@gmail.com>
2026-01-28 14:14:55 +0530

    map CORS fixing

--- backend1/routes/farmerRoutes.js
+++ backend1/routes/farmerRoutes.js
@@ -1,11 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authMiddleware = require('../middleware/authMiddleware');
-const farmerLocationController = require('../controllers/farmerLocationController');
+const authMiddleware = require("../middleware/authMiddleware");
+const farmerLocationController = require("../controllers/farmerLocationController");
 
-//  POST: Save location
-router.post('/location', authMiddleware, farmerLocationController.saveLocation);
-//  GET: Fetch location
-router.get('/location', authMiddleware, farmerLocationController.getLocation);
+// 🔥 CORS HEADER MIDDLEWARE (route-level, minimal)
+const allowCors = (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+};
+
+// POST: Save location
+router.post(
+  "/location",
+  allowCors,
+  authMiddleware,
+  farmerLocationController.saveLocation,
+);
+
+// GET: Fetch location
+router.get(
+  "/location",
+  allowCors,
+  authMiddleware,
+  farmerLocationController.getLocation,
+);
 
 module.exports = router;
