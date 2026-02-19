1b42d744a427442d057c7a36e08950c91792a35e
krishidev1 <ghoshkrishidev@gmail.com>
2026-02-19 17:48:07 +0530

    dashboard page showing

--- frontend-master/src/components/Dashboard.jsx
+++ frontend-master/src/components/Dashboard.jsx
@@ -5,13 +5,15 @@ import L from "leaflet";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
-
-
-
 import {
   Thermometer,
   Droplets,
   Sprout,
+  FlaskConical,
+  Beaker,
+} from "lucide-react";
+
+import {
   AlertTriangle,
   MapPin,
   Clock,
