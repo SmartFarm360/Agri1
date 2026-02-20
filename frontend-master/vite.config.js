96d1e77e4f5cf8ddc73c4a795c7bcaaf21b2db2e
krishidev1 <ghoshkrishidev@gmail.com>
2026-02-20 22:46:12 +0530

    css fixing and minimal comment changes

--- frontend-master/vite.config.js
+++ frontend-master/vite.config.js
@@ -3,10 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1200,
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:5000', 
     },
   },
 })
+
 
